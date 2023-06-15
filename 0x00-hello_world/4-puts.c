c7bf608f416ec0094d53f111577cf33bf742e73a
timothykinyi <kinyi9461@gmail.com>
2023-06-15 08:37:57 -0700

    L

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 
+/*prints the result from put function*/
 int main(void)
 {
-/*prints the result from put function*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
