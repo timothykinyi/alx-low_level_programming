8b049cf10a04ccdd26a296c4d96eb3c19f646ebb
timothykinyi <kinyi9461@gmail.com>
2023-06-15 09:50:17 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
-# include <stdio.h>
-
+#include <stdio.h>
 /**
- *main : a program that prints a sentence using printf
- *Return: Always 0 (Success)
- */
+* main - Entry point#
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
