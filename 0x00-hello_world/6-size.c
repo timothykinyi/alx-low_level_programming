a3afcc3630e6419996f671fb8e32761de9c385dc
timothykinyi <kinyi9461@gmail.com>
2023-06-15 10:41:32 -0700

    l

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+* main - Entry point#
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Size of a char: %zu\n",sizeof(char));
+printf("Size of an int: %zu\n",sizeof(int));
+printf("Size of a long int: %zu\n",sizeof(long));
+printf("Size of a long long int: %zu\n",sizeof(long long));
+printf("Size of a float %zu",sizeof(float));
+return (0);
+}
