b282d1a6c2114272032a67619dcbe9d883b80c4c
timothykinyi <kinyi9461@gmail.com>
2023-06-15 23:56:52 -0700

    if else project

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n < 0)
+{
+printf("is negative \n");
+}
+else if (n == 0)
+(
+printf("is zero");
+}
+else
+}
+printf("is positive";
+}
+return (0);
+}
