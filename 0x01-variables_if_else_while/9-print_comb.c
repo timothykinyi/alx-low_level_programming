f84ac5d96a5d3ca750b25d987ce2c70d1ef1060e
timothykinyi <kinyi9461@gmail.com>
2023-06-16 15:37:37 -0700

    if else project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char h;
+for (h = '0'; h <= '9'; h++)
+{
+putchar(h);
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
