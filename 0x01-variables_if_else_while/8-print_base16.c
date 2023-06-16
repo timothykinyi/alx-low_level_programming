7aa46055bd904dde9009f7e1c51e77fe02b964f2
timothykinyi <kinyi9461@gmail.com>
2023-06-16 15:16:03 -0700

    if else project

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,14 @@
 int main(void)
 {
 char h;    
-for (h = '0'; h <= '9'; h++) {
+for (h = '0'; h <= '9'; h++)
+{
 putchar(h);
 }    
-for (h = 'a'; h <= 'f'; h++) {
+for (h = 'a'; h <= 'f'; h++)
+{
 putchar(h);
-}   
+}
 putchar('\n');
-return 0;
+return (0);
 }
