fe18f5d884a156c192b720b7c6a7cb15445e37d4
timothykinyi <kinyi9461@gmail.com>
2023-06-16 14:19:09 -0700

    if else project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n < 0)
 {
-  printf("%d is negative \n", n);
+  printf("%d is negative\n", n);
 }
 else if (n == 0)
 {
-  printf("%d is zero", n);
+  printf("%d is zero\n", n);
 }
 else
 {
-  printf("%d is positive", n);
+  printf("%d is positive\n", n);
 }
 return (0);
 }
