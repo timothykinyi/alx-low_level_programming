bf320b6cb25fe4a7ab4a0b7f96dc0fc2086b8d1b
timothykinyi <kinyi9461@gmail.com>
2023-06-16 14:45:33 -0700

    if else project

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)                                                                                                         
+ */
+int main(void)
+{
+char character
+for (character = 'a' ; character <= 'z' ; character++)
+{
+if((character != 'q') || (character != 'e'))
+{
+putchar(character);
+}
+}
+putchar('\n');
+return (0);
+}
