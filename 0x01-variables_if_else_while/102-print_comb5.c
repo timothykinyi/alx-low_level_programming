09f10b256f3b7da87eeeb51509647d5c7787e507
timothykinyi <kinyi9461@gmail.com>
2023-06-16 19:54:57 -0700

    if else project

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number1, number2;
+for (number1 = 0; number1 <= 99; number1++)
+{
+for (number2 = number1; number2 <= 99; number2++)
+{
+int tens1 = number1 / 10;
+int ones1 = number1 % 10;
+int tens2 = number2 / 10;
+int ones2 = number2 % 10;
+
+putchar('0' + tens1);
+putchar('0' + ones1);
+putchar(' ');
+putchar('0' + tens2);
+putchar('0' + ones2);
+if (number1 != 99 || number2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchat('\n');
+return (0);
+}
