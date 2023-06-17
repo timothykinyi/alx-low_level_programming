8e1ceae5f07a9ec169f4cb26f6e4f9f955792ecd
timothykinyi <kinyi9461@gmail.com>
2023-06-16 20:09:02 -0700

    if else project

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,13 +9,12 @@ int main(void)
 int number1, number2;
 for (number1 = 0; number1 <= 99; number1++)
 {
-for (number2 = number1; number2 <= 99; number2++)
+for (number2 = (number1 == 0) ? 1 : number1; number2 <= 99; number2++)
 {
 int tens1 = number1 / 10;
 int ones1 = number1 % 10;
 int tens2 = number2 / 10;
 int ones2 = number2 % 10;
-
 putchar('0' + tens1);
 putchar('0' + ones1);
 putchar(' ');
