75971911c12fde0fe02d6514ea8965b2d0c0a824
timothykinyi <kinyi9461@gmail.com>
2023-06-19 09:07:55 -0700

    if else project

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,19 +7,23 @@
 int main(void)
 {
 int i, j;
+int count = 0;
 for (i = 0; i <= 99; i++)
 {
 for (j = i; j <= 99; j++)
 {
 if (i != j)
 {
+if (count > 0)
+{
+putchar(',');
+putchar(' ');
+}
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-putchar(',');
-putchar(' ');
 }
 }
 }
