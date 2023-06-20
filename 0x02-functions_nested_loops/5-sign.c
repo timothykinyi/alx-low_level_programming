56bab97fa611a224084992fb743087eba088daf7
timothykinyi <kinyi9461@gmail.com>
2023-06-20 12:24:55 -0700

    todays

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include "main.h"
 # include <stdio.h>
 /**
  * main - Entry point
@@ -8,7 +9,7 @@ int print_sign(int n)
 {
 if (n>0)
 {
-printf("+");
+_putchar("43");
 return (1);
 }
 else if (n==0)
