64cf3417a647d7acd90df3775b53dcb66e77fcbe
timothykinyi <kinyi9461@gmail.com>
2023-06-20 16:49:10 -0700

    todays

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,41 +2,19 @@
 
 void times_table(void)
 {
-int s = 0, e = 9, count = 0;
-while (count <= 9)
+int x, y, z=0;
+for (x=0;x < 10;x++;)
 {
-while (s <= e)
+for(y=0;y < 10;y++)
 {
-int product = count * s;
-if (s != 0)
+z=x*y;
+_putchar(x);
+if (y != 9)
 {
 _putchar(',');
-if (product < 10)
-{
-_putchar(' ');
 _putchar(' ');
 }
-else
-{
- _putchar(' ');
 }
-if (product < 10)
-{
-_putchar(product + '0');
-}
-else
-{
-_putchar(product / 10 + '0');
-_putchar(product % 10 + '0');
-}
-if (s == e)
-{
 _putchar('\n');
-s++;
-}
-s= 0;
-count++;
-}
-}
 }
 }
