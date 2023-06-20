ec43ffa4e701f0f4efa223ea4a40ac8d975800cd
timothykinyi <kinyi9461@gmail.com>
2023-06-20 15:27:23 -0700

    todays

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,27 +5,25 @@ void times_table(void)
 int x, y, z, u, d;
 for (x = 0; x <= 9; x++)
 {
+_putchar('0');
+_putchar(',');
+_putchar(" ");
 for (y = 0;y <= 9;y++)
 {
-z = x+y;
-if (z > 9)
+z = x*y;
+if ((z/10)> 0)
 {
-u = z % 10;
-d =(z - u) / 10;
-_putchar(44);
-_putchar(32);
-_putchar(d + '0');
-_putchar(u + '0');
+_putchar((d / 10)+'0');
 }
 else
 {
-if (y != 0)
-{
-_putchar(44);
-_putchar(32);
-_putchar(32);
+_putchar(' ');
 }
-_putchar(z + '0');
+ _putchar((d % 10) + '0');
+if (y < 9)
+{
+_putchar(',');
+_putchar(' ');
 }
 }
 _putchar('\n');
