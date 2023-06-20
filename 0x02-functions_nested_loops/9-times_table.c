19bee07dff6e73d92c6981434433cb6b60c09027
timothykinyi <kinyi9461@gmail.com>
2023-06-20 15:36:41 -0700

    todays

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,8 +5,10 @@ void times_table(void)
 {
 int rows = 10;
 int cols = 10;
-for (int i = 0; i < rows; i++) {
-for (int j = 0; j < cols; j++) {
+for (int i = 0; i < rows; i++)
+{
+for (int j = 0; j < cols; j++)
+{
 printf("%2d, ", i * j);
 }
 printf("\n");
