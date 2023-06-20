e18867b36294a546d7d08e1b797f893ca9e61347
timothykinyi <kinyi9461@gmail.com>
2023-06-20 10:52:59 -0700

    todays

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,28 +1,22 @@
 # include <stdio.h>
+# include "main.h"
 /**
  * main - Entry point
  * 
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
-int main(void)
-{
-int x=0;
-while(x<10)
-{
-print_alphabet_x10();
-x++;
-}
-return (0);
-}
-
-
 void print_alphabet_x10(void)
 {
+int x=0;
 char lower;
+while(x<10)
+{
 for(lower = 'a';lower <= 'z';lower++)
 {
 putchar(lower);
 }
 putchar('\n');
+x++;
+}
+return (0);
 }
