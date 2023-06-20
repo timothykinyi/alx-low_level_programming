234d84167d86fd2ed1760dc374e10c9021c6dad0
timothykinyi <kinyi9461@gmail.com>
2023-06-20 11:42:39 -0700

    todays

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,25 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-int print_sign(int n);
-int main(void)
-{
-int n=2;
-print_sign(n);
-if (n>0)
-{
-return (1);
-}
-else if (n==0)
-{
-return (0);
-}
-else if (n<0)
-{
-return (-1);
-}
-}
-
 int print_sign(int n)
 {
 if (n>0)
