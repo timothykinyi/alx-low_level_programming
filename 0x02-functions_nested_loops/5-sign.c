50aa050a14ab8780f6041826c3ea84fce1f3b056
timothykinyi <kinyi9461@gmail.com>
2023-06-20 12:35:01 -0700

    todays

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 if (n>0)
 {
-putchar(43);
+_putchar(43);
 return (1);
 }
 else if (n==0)
 {
-printf("0");
+_putchar(45);
 return (0);
 }
 else if (n<0)
 {
-printf("-");
+_putchar(48);
 return (-1);
 }
 return (0);
