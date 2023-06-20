5da162a438060cd1f9f4d8ea860b0b3734e8f796
timothykinyi <kinyi9461@gmail.com>
2023-06-20 13:32:27 -0700

    todays

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,14 @@
 
 int print_last_digit(int c)
 {
-int last = c%10;
-_putchar(last);
-return (0);
+int last = c % 10;
+if (last < 0)
+{
+_putchar(-last + 48);
+return (-last);
+}
+else
+{
+_putchar(last + 48);
+return (last);
 }
