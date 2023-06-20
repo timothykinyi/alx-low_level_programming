57ca738bc8911020d2aefb77bcb87415fba82a1b
timothykinyi <kinyi9461@gmail.com>
2023-06-20 11:39:09 -0700

    todays

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,29 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int _isalpha(int c);
-int main(void)
-{
-int c= 'o';
-int k=_isalpha(c);
-if (k==1)
-{
-return (1);
-}else if (k==0)
-{
-return (0);
-}
-}
-
-
 int _isalpha(int c)
 {
 if (isalpha(c))
 {
 return (1);
-}else
+}
+else
 {
 return (0);
 }
-
 }
