87b6d633f2df2e3fcadf931f4615721f86f3e354
timothykinyi <kinyi9461@gmail.com>
2023-06-21 06:14:19 -0700

    todays

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,9 @@
 # include "main.h"
-
+/**
+ * print_last_digit - Entry point
+ * @c : character to be cheacked
+ * Return: Always 0 (Success)
+ */
 int print_last_digit(int c)
 {
 int last = c % 10;
