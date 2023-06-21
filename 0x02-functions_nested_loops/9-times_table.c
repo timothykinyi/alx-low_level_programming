e67f1c3df455e98e050443b51974e0edeaff43de
timothykinyi <kinyi9461@gmail.com>
2023-06-21 06:36:57 -0700

    todays

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,8 +7,8 @@
  */
 void times_table(void)
 {
-int x, l = 0, y;
-int k = 0, z;
+int x, y;
+int z;
 for (x= 0; x < 10; x++)
 {
 k = 0;
@@ -18,14 +18,7 @@ z = (x * y);
 printf("%d", z);
 if (y != 9)
 {
-if ((l == 1) || (l == 0))
-{
-printf(",");
-printf("  ");
-}
-else if (l == 2)
-{
-if(k < 4)
+if (z < 10)
 {
 printf(",");
 printf("  ");
@@ -34,57 +27,11 @@ else
 {
 printf(",");
 printf(" ");
-}
-k++;
-}
-else if (l == 3)
-{
-if(k < 3)
-{
-printf(",");
-printf("  ");
-}
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
-else if (l == 4)
-{
-if(k < 2)
-{
-printf(",");
-printf("  ");
-}
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
-else if (l >= 5)
-{
-if(k < 1)
-{
-printf(",");
-printf("  ");
-}
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
 }
 else
 {
 printf("\n");
 }
 }
-l++;
 }
 }
