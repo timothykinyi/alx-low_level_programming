f6cc8c954c4d8ddb63d09d4c6a6fc10dd597d0a3
timothykinyi <kinyi9461@gmail.com>
2023-06-21 06:17:29 -0700

    todays

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,20 @@
 # include "main.h"
 # include <stdio.h>
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void jack_bauer(void)
 {
 int w, x, y, z;
-for(w=0;w<=2;w++)
+for (w = 0; w <= 2; w++)
 {
-for(x=0;x<10;x++)
+for (x = 0; x < 10; x++)
 {
-for(y=0;y<6;y++)
+for (y = 0; y < 6; y++)
 {
-for(z=0;z<10;z++)
+for (z = 0; z < 10; z++)
 {
 if (w >= 2 && x >= 4)
 {
