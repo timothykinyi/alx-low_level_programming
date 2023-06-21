86115323c2a11cb50c83af1eac21b50318692e50
timothykinyi <kinyi9461@gmail.com>
2023-06-21 12:39:32 -0700

    todays

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,15 @@
 # include <stdio.h>
-
+/**
+ * times_table - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int k, total = 0;
 for (k = 0; k < 1024; k++)
 {
-if(((k % 3) == 0) || ((k % 5) == 0))
+if (((k % 3) == 0) || ((k % 5) == 0))
 {
 total += k;
 }
