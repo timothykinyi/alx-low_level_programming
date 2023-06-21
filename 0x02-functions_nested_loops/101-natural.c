da6450ed3628aa57d0cf861a9a275514d99340cc
timothykinyi <kinyi9461@gmail.com>
2023-06-21 12:32:18 -0700

    todays

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,93 +1,15 @@
-# include "main.h"
 # include <stdio.h>
-void print_times_table(int n)
+
+int main(void)
 {
-int x, l=0, y;
-int k = 0, z;
-for (x=n;x <= n;x++)
+int k, total = 0;
+for (k = 0; k < 1024; k++)
 {
-k = 0;
-for(y=n;y <= n;y++)
+if(((k % 3) = 0) || ((k % 5) = 0))
 {
-z = (x*y);
-printf("%d", z);
-if (y != 9)
-{
-if ((l == 1) || (l == 0))
-{
-printf(",");
-printf("  ");
+total + k;
 }
-else if (l == 2)
-{
-if(k < 4)
-{
-printf(",");
-printf("  ");
+printf("%d\n", total);
 }
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
-else if (l == 3)
-{
-if(k < 3)
-{
-printf(",");
-printf("  ");
-}
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
-else if (l == 4)
-{
-if(k < 2)
-{
-printf(",");
-printf("  ");
-}
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
-else if (l >= 5)
-{
-if(k < 1)
-{
-printf(",");
-printf("  ");
-}
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
-}
-else
-{
-printf(",");
-printf(" ");
-}
-k++;
-}
-}
-else
-{
-printf("\n");
-}
-}
-l++;
-}
+return (0);
 }
