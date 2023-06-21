3bf871be36e2eac0c64a4128995f054e58630f61
timothykinyi <kinyi9461@gmail.com>
2023-06-21 12:59:49 -0700

    todays

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+int n = 50;
+unsigned long long fib[n];
+fib[0] = 1; 
+fib[1] = 2;
+for (int i = 2; i < n; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+for (int i = 0; i < n; i++)
+{
+printf("%llu", fib[i]);
+if (i != n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+return 0;
+}
