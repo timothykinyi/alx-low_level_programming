854abd508353f24c4a90a1883de59ce54dbcbf10
timothykinyi <kinyi9461@gmail.com>
2023-06-21 06:12:52 -0700

    todays

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,17 @@
 # include "main.h"
-
+/**
+ * _abs - Entry point
+ * @i : the number to be evaluated
+ * Return: Always 0 (Success)
+ */
 int _abs(int i)
 {
-if (i>= 0)
+if (i >= 0)
 {
-return(i);
+return (i);
 }
 else
 {
-return (i*-1);
+return (i * -1);
 }
 }
