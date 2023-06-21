399020bce44c47f83ad622401a880547a1accee3
timothykinyi <kinyi9461@gmail.com>
2023-06-21 06:07:24 -0700

    todays

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 # include <stdio.h>
 /**
- * main - Entry point
- *
+ * print_sign - Entry point
+ * @n : number to be checked
  * Return: Always 0 (Success)
  */
 int print_sign(int n)
