ac932f6a71c4fa30b548d5a8657fe77aa3fbb09f
timothykinyi <kinyi9461@gmail.com>
2023-06-21 02:26:31 -0700

    todays

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@
 
 void print_alphabet_x10(void)
 {
-int x=0;
+int x = 0;
 char lower;
-while(x<10)
+while (x < 10)
 {
-for(lower = 'a';lower <= 'z';lower++)
+for (lower = 'a'; lower <= 'z'; lower++)
 {
 putchar(lower);
 }
