ef1ef49b0ed5accbd4364ea4dd8e22ffc2c4f74e
timothykinyi <kinyi9461@gmail.com>
2023-06-21 07:07:05 -0700

    todays

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,11 @@
 # include <stdio.h>
 # include "main.h"
-
+/**
+ * add - Entry point
+ * @a : number to be added
+ * @b : number to be added
+ * Return: Always 0 (Success)
+ */
 int add(int a, int b)
 {
 return (a + b);
