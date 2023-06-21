bd1accac3da18810855e89833b4e0fdf63daa89c
timothykinyi <kinyi9461@gmail.com>
2023-06-21 12:33:53 -0700

    todays

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,9 +5,9 @@ int main(void)
 int k, total = 0;
 for (k = 0; k < 1024; k++)
 {
-if(((k % 3) = 0) || ((k % 5) = 0))
+if(((k % 3) == 0) || ((k % 5) == 0))
 {
-total + k;
+total += k;
 }
 printf("%d\n", total);
 }
