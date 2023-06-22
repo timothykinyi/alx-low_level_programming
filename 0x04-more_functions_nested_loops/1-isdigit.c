8a2bb86e68a6c60cd6b95b95d6881c1301a01731
timothykinyi <kinyi9461@gmail.com>
2023-06-22 09:59:17 -0700

    todays

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 # include "main.h"
 /**
- * isdigit - Entry point
+ * _isdigit - Entry point
  * @c : number to be cheacked
  * Return Always 0 (success)
  */
 int _isdigit(int c)
 {
-if ((c >= 48) && (c <=57))
+if (c >= 48 && c <=57)
 return (1);
 else
 return (0);
