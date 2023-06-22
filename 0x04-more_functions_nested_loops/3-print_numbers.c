7baf4bd0e38f468861586e51e25bac9f7f1a87b5
timothykinyi <kinyi9461@gmail.com>
2023-06-22 10:31:05 -0700

    todays

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,19 +2,14 @@
 /**
  * print_numbers - Entry point
  *
- * Return Always 0 (success)
+ * Return : Always 0 (success)
  */
 void print_numbers(void)
 {
-_putchar('0');
-_putchar('1');
-_putchar('2');
-_putchar('3');
-_putchar('4');
-_putchar('5');
-_putchar('6');
-_putchar('7');
-_putchar('8');
-_putchar('9');
+int x;
+for (x = 0; x <= 9; x++)
+{
+_putchar (x + '0' );
+}
 _putchar('\n');
 }
