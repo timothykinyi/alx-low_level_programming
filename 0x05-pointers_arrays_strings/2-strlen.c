4c20370997b7465414da5b97208d046372f790a8
timothykinyi <kinyi9461@gmail.com>
2023-06-26 07:31:47 -0700

    todays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+# include "main.h"
+# include <string.h>
+/**
+ * _strlen - gives the length of a tring
+ * @s: characters to be counted
+ * Return : noting
+ */
+int _strlen(char *s)
+{
+size_t length = strlen(*s);
+}
