f35363a96cfa035feceed4e922ba75acfba1cce3
timothykinyi <kinyi9461@gmail.com>
2023-06-26 05:13:42 -0700

    todays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+ * swap_int - swaps two numbers
+ * @a: first number to be swaped
+ * @b: second number to be swaped
+ * return : nothing
+ */
+void swap_int(int *a, int *b)
+{
+int s, d;
+s = *a;
+b = *b;
+*a = &b;
+*b = $a;
+}
