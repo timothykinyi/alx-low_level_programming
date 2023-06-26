09538e127fd62dc232eddb0a41ce3eda85fa5456
timothykinyi <kinyi9461@gmail.com>
2023-06-26 05:04:02 -0700

    todays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 # include "main.h"
 /**
- * reset_to_98 - Entry point 
- * @n pointer to a number
+ * reset_to_98 - Entry point
+ * @*n: pointer to a number
  * return : 0
  */
 void reset_to_98(int *n)
