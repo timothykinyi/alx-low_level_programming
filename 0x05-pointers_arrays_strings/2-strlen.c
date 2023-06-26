1bf9165d506d5246a56f571efc836c8c6ce6b3d5
timothykinyi <kinyi9461@gmail.com>
2023-06-26 07:42:57 -0700

    todays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - gives the length of a tring
  * @s: characters to be counted
- * Return : noting
+ * Return : length of the *s
  */
 int _strlen(char *s)
 {
@@ -13,5 +13,5 @@ while (*s != '\0')
 length++;
 s++;
 }
-return length;
+return (length);
 }
