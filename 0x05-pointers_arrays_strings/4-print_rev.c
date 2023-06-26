c0aa660d0c7ebf877e559cbb9e383f23d3020721
timothykinyi <kinyi9461@gmail.com>
2023-06-26 10:03:11 -0700

    todays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,9 @@
 void print_rev(char *s)
 {
 int length = strlen(s);
-for (int i = length - 1; i >= 0; i--)
+for(int i = length - 1; i >= 0; i--)
 {
-printf("%c", s[i]);
+_putchar(s[i]);
 }
-printf("\n");
+_putchar('\n');
 }
