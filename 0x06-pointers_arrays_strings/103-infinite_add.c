7412e460f899bf289fbaa47d38a16fa16d059068
timothykinyi <kinyi9461@gmail.com>
2023-06-29 13:08:57 -0700

    todays

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,22 +8,22 @@
 
 void rev_string(char *n)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
+int i = 0;
+int j = 0;
+char temp;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+while (*(n + i) != '\0')
+{
+i++;
+}
+i--;
 
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
 }
 
 /**
