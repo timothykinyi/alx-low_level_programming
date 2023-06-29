53b87e249d046183a229a67a806872d09492e546
timothykinyi <kinyi9461@gmail.com>
2023-06-29 12:18:18 -0700

    todays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-strcat(*dest, *src);
+int i = 0,j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
 return (dest);
 }
